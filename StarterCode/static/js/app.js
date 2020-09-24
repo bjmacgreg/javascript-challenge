a5eef1d6c859f3a47a04d5ab7dd7a3429371884a
Barbara MacGregor <1453-macgregb@users.noreply.umn.bootcampcontent.com>
2020-09-24 02:04:55 -0500

    Initial

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,9 @@
 // Reference for data from data.js
 var tableData = data;
-let selectedData = data;
+//var selectedData = tableData;
+var dateInput = "888";
+var cityInput = "888";
+var stateInput = "888"; 
 // Reference for button
 var button = d3.select("#filter-btn");
 // Get a reference to the table body
@@ -8,85 +11,77 @@ var tbody = d3.select("tbody");
 // Console.log each sighting
 console.log(data);
 
+
 //Function to display selected data in table
 function fillTable(x) {
 //Clear existing data
-  d3.select("#ufo-table tbody").selectAll("tr").remove();  
+    d3.select("#ufo-table tbody").selectAll("tr").remove();  
 //Add rows and fill them with data (assuming all data organized the same way)
-  x.forEach((ufoSighting) => {  
-  //x.forEach((ufoSighting) => {
-      let row = tbody.append("tr");
-      Object.entries(ufoSighting).forEach(([, value]) => {
-        let cell = row.append("td");
-        cell.text(value);
-      });
-  });
+    x.forEach((ufoSighting) => {  
+        let row = tbody.append("tr");
+        Object.entries(ufoSighting).forEach(([, value]) => {
+            let cell = row.append("td");
+            cell.text(value);
+        });
+    });
 }
 
 //Display complete dataset
-fillTable(selectedData);
+fillTable(tableData);
 
-// Complete event handler function for form
+//EVENT HANDLER
 button.on("click", function() {
-  let cityInput = null;
-  let dateInput = null;
+  //declare selected data variable
+  //var selectedData = data;
+  //list of inputs
+  //inputs = [dataInput, cityInput];
+  //set inputs to "none"
+  //let dateInput = "888";
+  //let cityInput = "888";
+  //let stateInput = "888"; 
+  //let selectedData1 = tableData; 
+  //let selectedData2 = tableData; 
+  //let selectedData3 = tableData; 
+  //collect inputs and filter data
+  dateInput = d3.select("#datetime-input").property ("value");
+  cityInput = d3.select("#city-input").property ("value"); 
+  stateInput = d3.select("#state-input").property ("value"); 
+
+  // console.log(dateInput);
+  // if (dateInput != "888") {selectedData1 = tableData.filter(tableData => (tableData.datetime === dateInput))}
+  // else {selectedData1 = tableData};
+  //fillTable(selectedData);
+  // console.log(selectedData1);
   
-      // Select the input element and get the requested data
-  dateInput = d3.select("#datetime-input").property ("value"); 
-  console.log(dateInput);
-  cityInput = d3.select("#city-input").property ("value");     
-  console.log(cityInput);
-  if (dateInput != null && cityInput != null){selectedData = tableData.filter(tableData => tableData.datetime === dateInput && tableData.city === cityInput)}
-  else if (dateInput != null){selectedData = tableData.filter(tableData => tableData.datetime === dateInput)}
-  else {selectedData = tableData.filter(tableData => tableData.city === cityInput)};
-  // if (dateInput != null && cityInput != null){selectedData = tableData.filter(tableData => tableData.datetime === dateInput && tableData.city === cityInput)};
+  // console.log(cityInput);
+  // if (cityInput != "888") {selectedData2 = selectedData1.filter(selectedData1 => (selectedData1.city === cityInput))}
+  // else {selectedData2 = selectedData1};   
+  // //fillTable(selectedData); 
+  // console.log(selectedData2); 
+
+  // console.log(cityInput);
+  // if (cityInput = "888") {selectedData2 = selectedData1}
+  // else {selectedData2 = selectedData1.filter(selectedData1 => (selectedData1.city === cityInput))};  
+  //fillTable(selectedData); 
+  // console.log(selectedData2); 
+
+
   
-  //let citySelection = tableData.filter(tableData => tableData.city === cityInput);
+  // console.log(stateInput);
+  // if (stateInput = "888") {selectedData3 = selectedData2}
+  // else {selectedData3 = selectedData2.filter(selectedData2 => (selectedData2.state === stateInput))};
+  //else selectedData = selectedData;
+  //else {selectedData = selectedData};  
 
-      // Output file of filtered data
-  console.log(selectedData);
+  if (dateInput != "888") {selectedData1 = tableData.filter(tableData => (tableData.datetime === dateInput))}
+  else {selectedData1 = tableData};
 
-  //selectedData = (tableData => tableData.datetime === dateSelection && tableData.city === cityInput)
-  fillTable(selectedData);   
-  //fillTable(selectedData);    
-  });
+  if (cityInput = "888") {selectedData2 = selectedData1}
+  else {selectedData2 = selectedData1.filter(selectedData1 => (selectedData1.city === cityInput))}; 
 
-  //const intersection = array1.filter(element => array2.includes(element));
-      // Prevent the page from refreshing
-  //d3.event.preventDefault();    
+  if (stateInput = "888") {selectedData3 = selectedData2}
+  else {selectedData3 = selectedData2.filter(selectedData2 => (selectedData2.state === stateInput))};
 
-//button.on("click",runEnter);
-//tbody.remove;
-//fillTable(selectedData);
-//form.on("submit",runEnter);
-
-// Select button
-
-
-// Select form
-//var form = d3.select("#form");
-// Create event handler for button
-
-  // users = users.filter(obj => obj.name == filter.name && obj.address == filter.address)
-
-//runEnter(selectedData);
-
-//form.on("submit",runEnter);
-// let bigCities = cities.filter(city => city.population > 3000000);
-// console.log(bigCities);
-
-
-// // first filter
-// function filterRating(hotel) {
-//   return hotel.rating >= filters.rating;
-// }
-
-// // second filter
-// function filterMeal(hotel) {
-//   return !filters.mealType.length || hotel.mealType == filters.mealType;
-// }
-
-// // apply both filters to initial array
-// function update() {
-//   let filteredCards = hotels.filter(filterRating).filter(filterMeal);
-// };
+  //fillTable(selectedData1)});
+  fillTable(selectedData2)});
+  //fillTable(selectedData3)});
